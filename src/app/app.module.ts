706fbc9af0c12148f17f14a780d180c10943a01a
Mateusz Jajkowicz <mateusz.jajkowicz@gmail.com>
2022-10-30 14:43:37 +0100

    reducing main.js raw size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
-
 import { environment } from "../environments/environment";
-// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-// import { provideAuth,getAuth } from '@angular/fire/auth';
-// import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire/compat';
-// import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-// import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
@@ -28,17 +20,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     BrowserAnimationsModule,
     SharedModule,
     AngularFireModule.initializeApp(environment.firebase),
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideAuth(() => getAuth()),
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideFirestore(() => getFirestore()),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
-    // provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]

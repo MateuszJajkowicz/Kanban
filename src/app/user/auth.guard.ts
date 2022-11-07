a7a280dfe9a20f33dd009fc82064dade6b6e3216
Mateusz <mateusz.jajkowicz@gmail.com>
2022-11-07 10:08:19 +0100

    Changed authGuard

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { SnackService } from '../services/snack.service';
-import { map, Observable, take, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,18 +13,26 @@ export class AuthGuard implements CanActivate {
     private snack: SnackService,
   ) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-        return this.afAuth.authState
-          .pipe(
-            take(1),
-            map(user => !!user),
-            tap(
-              loggedIn => {
-                if (!loggedIn) {
-                  this.snack.authError();
-                }
-              }
-            )
-          );
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+        // return this.afAuth.authState
+        //   .pipe(
+        //     take(1),
+        //     map(user => !!user),
+        //     tap(
+        //       loggedIn => {
+        //         if (!loggedIn) {
+        //           this.snack.authError();
+        //         }
+        //       }
+        //     )
+        //   );
+        return this.afAuth.user.pipe(
+          map((user): boolean => !!user),
+          tap((loggedIn: boolean) => {
+            if (!loggedIn) {
+              this.snack.authError();
+            }
+          })
+        );
       }
 }
